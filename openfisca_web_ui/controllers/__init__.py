2bb865cf1cbd79ff0fd35ae4ac8e6b1b19f271fb
Emmanuel Raviart <emmanuel@raviart.com>
2014-02-16 10:12:49 +0100

    When user rejects cookies redirect him to OpenFisca project site.

--- openfisca_web_ui/controllers/__init__.py
+++ openfisca_web_ui/controllers/__init__.py
@@ -42,7 +42,8 @@ def accept_cookies(req):
     ctx = contexts.Ctx(req)
     if req.method == 'POST':
         if not ('accept' in req.params and conv.check(conv.guess_bool(req.params.get('accept-checkbox'))) is True):
-            return templates.render(ctx, '/reject-cookies.mako')
+            # User doesn't accept the use of cookies => Bye bye.
+            return wsgihelpers.redirect(ctx, location = conf['www.url'])
         session = ctx.session
         if session is None:
             session = ctx.session = model.Session()
