9a0c8dae95233c59d42222844fb1658c2a54b733
Romain Soufflet <rsoufflet@easter-eggs.com>
2014-02-19 18:24:33 +0100

    Add "Open-all" and "Close-all" button.

--- openfisca_web_ui/static/js/legislation.js
+++ openfisca_web_ui/static/js/legislation.js
@@ -10,4 +10,16 @@ require([
         evt.preventDefault();
     });
 
+    $('.btn-toggle-open').on('click', function(evt) {
+        $('.collapse-node-toggle.collapsed').each(function(index, el) {
+            $(this).trigger('click');
+        });
+    });
+    $('.btn-toggle-close').on('click', function(evt) {
+        $('.collapse-node-toggle').each(function(index, el) {
+            if (! $(this).hasClass('collapsed')) {
+                $(this).trigger('click');
+            }
+        });
+    });
 });
