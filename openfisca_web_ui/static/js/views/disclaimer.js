29876c9a252717eb040bf2e4ddc5761d0d5fa29c
Christophe Benz <cbenz@easter-eggs.com>
2014-02-25 10:17:57 +0100

    Add missing ';'.

--- openfisca_web_ui/static/js/views/disclaimer.js
+++ openfisca_web_ui/static/js/views/disclaimer.js
@@ -3,8 +3,7 @@ define(['jquery'], function ($) {
 	function init (config) {
 		$('.alert.disclaimer').on('close.bs.alert', function () {
 			$.get(config.disclaimerClosedUrlPath);
-		})
-
+		});
 	}
 
 	return {init: init};
