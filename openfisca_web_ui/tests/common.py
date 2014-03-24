608929287845bd5c01bb7e20d6a0652d1b264b81
Christophe Benz <christophe.benz@data.gouv.fr>
2014-03-24 14:44:25 +0100

    Use test.ini.

--- openfisca_web_ui/tests/common.py
+++ openfisca_web_ui/tests/common.py
@@ -16,6 +16,6 @@ class TestCaseWithApp(unittest.TestCase):
     def setUp(self):  # noqa
         global app
         if app is None:
-            conf_file_path = os.path.abspath(os.environ.get(u'PASTE_INI', u'development.ini'))
+            conf_file_path = os.path.abspath(os.environ.get(u'PASTE_INI', u'test.ini'))
             app = loadapp(u'config:{}#main'.format(conf_file_path))
             environment.setup_environment()
