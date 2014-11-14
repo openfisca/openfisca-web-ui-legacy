0a0f969490f3a433943c74f9f5cc6930b2dbcdf3
Christophe Benz <christophe.benz@data.gouv.fr>
2014-11-14 16:03:57 +0100

    Rename PLF and add tooltip

--- openfisca_web_ui/static/js/components/visualizations/reform-selector.js
+++ openfisca_web_ui/static/js/components/visualizations/reform-selector.js
@@ -4,6 +4,8 @@
 var React = require('react/addons'),
   ReactIntlMixin = require('react-intl');
 
+var Tooltip = require('../tooltip');
+
 var cx = React.addons.classSet;
 
 
@@ -24,12 +26,14 @@ var ReformSelector = React.createClass({
         <button className={classes(null)} onClick={() => this.props.onChange(null)}>
           {this.getIntlMessage('reference')}
         </button>
-        <button
-          className={classes('plf2015')}
-          onClick={() => this.props.onChange('plf2015')}
-          title='Projet de loi de financement rectificative de la sécurité sociale'>
-          PLF 2015
-        </button>
+        <Tooltip>
+          <button
+            className={classes('plf2015')}
+            onClick={() => this.props.onChange('plf2015')}
+            title='Projet de loi de financement rectificative'>
+            PLFR 2014
+          </button>
+        </Tooltip>
         <button className={classes('plf2015-diff')} onClick={() => this.props.onChange('plf2015-diff')}>
           {this.getIntlMessage('difference')}
         </button>
