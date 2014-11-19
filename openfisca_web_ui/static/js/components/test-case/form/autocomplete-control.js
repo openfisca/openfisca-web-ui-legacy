4a0772bf76f21605b067c1a2188e8397302c8523
Christophe Benz <christophe.benz@data.gouv.fr>
2014-11-19 14:58:56 +0100

    Convert tabs to spaces

--- openfisca_web_ui/static/js/components/test-case/form/autocomplete-control.js
+++ openfisca_web_ui/static/js/components/test-case/form/autocomplete-control.js
@@ -2,79 +2,79 @@
 'use strict';
 
 var Autocomplete = require('ron-react-autocomplete'),
-	React = require('react'),
-	ReactIntlMixin = require('react-intl');
+  React = require('react'),
+  ReactIntlMixin = require('react-intl');
 
 
 var AutocompleteControl = React.createClass({
-	mixins: [ReactIntlMixin],
-	propTypes: {
-		autocomplete: React.PropTypes.func.isRequired,
-		displayedValue: React.PropTypes.string,
-		error: React.PropTypes.string,
-		label: React.PropTypes.component.isRequired,
-		name: React.PropTypes.string.isRequired,
-		onChange: React.PropTypes.func.isRequired,
-		value: React.PropTypes.string,
-	},
-	handleChange: function(selectedItem) {
-		this.props.onChange({
-			displayedValue: selectedItem.title,
-			value: selectedItem.id,
-		});
-	},
-	handleDelete: function() {
-		this.props.onChange({
-			displayedValue: '',
-			value: null,
-		});
-	},
-	handleSearch: function(options, searchTerm, cb) {
-		var onError = (xhr, status, err) => {
-			if (this.isMounted()) {
-				cb(err);
-			}
-		};
-		var onSuccess = data => {
-			if (this.isMounted()) {
-				cb(null, data.data.items.map(item => {
-					return {
-						id: item.code,
-						title: item.main_postal_distribution, // jshint ignore:line
-					};
-				}))
-			}
-		};
-		this.props.autocomplete(searchTerm, onSuccess, onError);
-	},
-	render: function() {
-		return (
-			<div>
-				{this.props.label}
-				<div className='row'>
-					<div className='col-sm-6'>
-			      <Autocomplete
-							onChange={this.handleChange}
-							search={this.handleSearch}
-							value={{
-								id: this.props.value,
-								title: this.props.displayedValue,
-							}}
-						/>
-					</div>
-					<div className='col-sm-6'>
-						<button
-							className='btn btn-default'
-							disabled={this.props.value}
-							onClick={this.handleDelete}
-							type='button'>
-							{this.getIntlMessage('delete')}
-						</button>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  mixins: [ReactIntlMixin],
+  propTypes: {
+    autocomplete: React.PropTypes.func.isRequired,
+    displayedValue: React.PropTypes.string,
+    error: React.PropTypes.string,
+    label: React.PropTypes.component.isRequired,
+    name: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    value: React.PropTypes.string,
+  },
+  handleChange: function(selectedItem) {
+    this.props.onChange({
+      displayedValue: selectedItem.title,
+      value: selectedItem.id,
+    });
+  },
+  handleDelete: function() {
+    this.props.onChange({
+      displayedValue: '',
+      value: null,
+    });
+  },
+  handleSearch: function(options, searchTerm, cb) {
+    var onError = (xhr, status, err) => {
+      if (this.isMounted()) {
+        cb(err);
+      }
+    };
+    var onSuccess = data => {
+      if (this.isMounted()) {
+        cb(null, data.data.items.map(item => {
+          return {
+            id: item.code,
+            title: item.main_postal_distribution, // jshint ignore:line
+          };
+        }))
+      }
+    };
+    this.props.autocomplete(searchTerm, onSuccess, onError);
+  },
+  render: function() {
+    return (
+      <div>
+        {this.props.label}
+        <div className='row'>
+          <div className='col-sm-6'>
+            <Autocomplete
+              onChange={this.handleChange}
+              search={this.handleSearch}
+              value={{
+                id: this.props.value,
+                title: this.props.displayedValue,
+              }}
+            />
+          </div>
+          <div className='col-sm-6'>
+            <button
+              className='btn btn-default'
+              disabled={this.props.value}
+              onClick={this.handleDelete}
+              type='button'>
+              {this.getIntlMessage('delete')}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 });
 
 module.exports = AutocompleteControl;
--- openfisca_web_ui/static/js/polyfills.js
+++ openfisca_web_ui/static/js/polyfills.js
@@ -1,106 +1,106 @@
 function install() {
-	// ECMAScript 6 features
-	// Polyfills are loaded only if functions are not implemented already.
+  // ECMAScript 6 features
+  // Polyfills are loaded only if functions are not implemented already.
 
-	require('es5-ext/string/#/starts-with/implement');
+  require('es5-ext/string/#/starts-with/implement');
 
-	// Non ECMAScript standard features
-	// https://github.com/medikoo/es5-ext#non-ecmascript-standard-features
-	// Define only the array methods actually used in the application code.
-	// No need to check if they are already implemented since they are non-standard.
+  // Non ECMAScript standard features
+  // https://github.com/medikoo/es5-ext#non-ecmascript-standard-features
+  // Define only the array methods actually used in the application code.
+  // No need to check if they are already implemented since they are non-standard.
 
-	Object.defineProperty(
-		Array.prototype,
-		'contains',
-		{
-			configurable: true,
-			enumerable: false,
-			value: require('es5-ext/array/#/contains'),
-			writable: true,
-		}
-	);
+  Object.defineProperty(
+    Array.prototype,
+    'contains',
+    {
+      configurable: true,
+      enumerable: false,
+      value: require('es5-ext/array/#/contains'),
+      writable: true,
+    }
+  );
 
-	Object.defineProperty(
-		Array.prototype,
-		'diff',
-		{
-			configurable: true,
-			enumerable: false,
-			value: require('es5-ext/array/#/diff'),
-			writable: true,
-		}
-	);
+  Object.defineProperty(
+    Array.prototype,
+    'diff',
+    {
+      configurable: true,
+      enumerable: false,
+      value: require('es5-ext/array/#/diff'),
+      writable: true,
+    }
+  );
 
-	Object.defineProperty(
-		Array.prototype,
-		'flatten',
-		{
-			configurable: true,
-			enumerable: false,
-			value: require('es5-ext/array/#/flatten'),
-			writable: true,
-		}
-	);
+  Object.defineProperty(
+    Array.prototype,
+    'flatten',
+    {
+      configurable: true,
+      enumerable: false,
+      value: require('es5-ext/array/#/flatten'),
+      writable: true,
+    }
+  );
 
-	Object.defineProperty(
-		Array.prototype,
-		'intersection',
-		{
-			configurable: true,
-			enumerable: false,
-			value: require('es5-ext/array/#/intersection'),
-			writable: true,
-		}
-	);
+  Object.defineProperty(
+    Array.prototype,
+    'intersection',
+    {
+      configurable: true,
+      enumerable: false,
+      value: require('es5-ext/array/#/intersection'),
+      writable: true,
+    }
+  );
 
-	Object.defineProperty(
-		Array.prototype,
-		'last',
-		{
-			configurable: true,
-			enumerable: false,
-			value: require('es5-ext/array/#/last'),
-			writable: true,
-		}
-	);
+  Object.defineProperty(
+    Array.prototype,
+    'last',
+    {
+      configurable: true,
+      enumerable: false,
+      value: require('es5-ext/array/#/last'),
+      writable: true,
+    }
+  );
 
-	Object.defineProperty(
-		Array.prototype,
-		'uniq',
-		{
-			configurable: true,
-			enumerable: false,
-			value: require('es5-ext/array/#/uniq'),
-			writable: true,
-		}
-	);
+  Object.defineProperty(
+    Array.prototype,
+    'uniq',
+    {
+      configurable: true,
+      enumerable: false,
+      value: require('es5-ext/array/#/uniq'),
+      writable: true,
+    }
+  );
 
-	// Polyfill requestAnimationFrame
-	var raf = require('raf');
-	if ( ! window.requestAnimationFrame) {
-		window.requestAnimationFrame = raf;
-	}
+  // Polyfill requestAnimationFrame
+  var raf = require('raf');
+  if ( ! window.requestAnimationFrame) {
+    window.requestAnimationFrame = raf;
+  }
 }
 
 
 function isValid(element) {
-	// Polyfill for element.validity.valid.
-	if (element.hasOwnProperty('validity')) {
-		return element.validity.valid;
-	} else {
-		// Should be enhanced if needed.
-		return true;
-	}
+  // Polyfill for element.validity.valid.
+  if (element.hasOwnProperty('validity')) {
+    return element.validity.valid;
+  } else {
+    // Should be enhanced if needed.
+    return true;
+  }
 }
 
 
 function valueAsNumber(element) {
-	// Polyfill for element.valueAsNumber.
-	var value = element.hasOwnProperty('valueAsNumber') ? element.valueAsNumber : parseInt(element.value);
-	if (isNaN(value)) {
-		value = null;
-	}
-	return value;
+  // Polyfill for element.valueAsNumber.
+  var value = element.hasOwnProperty('valueAsNumber') ? element.valueAsNumber : parseInt(element.value);
+  if (isNaN(value)) {
+    value = null;
+  }
+  return value;
 }
 
 
