294fd8fb92a868800f5a3509c15b89273404b8b0
Christophe Benz <christophe.benz@data.gouv.fr>
2014-12-19 19:10:10 +0100

    Add reforms

--- openfisca_web_ui/static/js/components/visualizations/reform-selector.js
+++ openfisca_web_ui/static/js/components/visualizations/reform-selector.js
@@ -30,6 +30,8 @@ var ReformSelector = React.createClass({
           <option value=''>{this.getIntlMessage('noReform')}</option>
           <option value='plfrss2014'>PLFR 2014</option>
           <option value='landais_piketty_saez'>Landais Piketty Saez</option>
+          <option value='revenu_de_base_cotisations'>Cotisations revenu de base</option>
+          <option value='revenu_de_base_enfants'>Revenu de base enfants</option>
         </select>
         <div className='checkbox' style={{marginLeft: 10}}>
           <label>
