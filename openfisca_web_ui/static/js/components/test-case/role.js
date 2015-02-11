c5b7e75cbd0f6b8dfe7920ccb1c6fc59d6b345fd
Christophe Benz <christophe.benz@data.gouv.fr>
2015-02-11 17:02:32 +0100

    Disable add button too when test-case is disabled

--- openfisca_web_ui/static/js/components/test-case/role.js
+++ openfisca_web_ui/static/js/components/test-case/role.js
@@ -18,18 +18,27 @@ var Role = React.createClass({
   render() {
     var maxCardinality = this.props.maxCardinality;
     return (
-      <div className="list-group-item">
+      <div className="clearfix list-group-item">
+        {
+          (
+            typeof maxCardinality === 'undefined' || ! this.props.children ||
+              this.props.children.length < maxCardinality
+          ) && (
+            <button
+              className='btn btn-default btn-sm pull-right'
+              disabled={this.props.disabled}
+              onClick={(event) => {
+                  event.preventDefault();
+                  this.props.onCreateIndividuInEntity();
+              }}
+            >
+              {this.getIntlMessage('add')}
+            </button>
+          )
+        }
         <p>{this.props.label}</p>
         {this.props.error && <p className="text-danger">{this.props.error}</p>}
         {this.props.children}
-        {
-          ! this.props.disabled && (typeof maxCardinality === 'undefined' || ! this.props.children ||
-          this.props.children.length < maxCardinality) && (
-            <a href="#" onClick={(event) => { event.preventDefault(); this.props.onCreateIndividuInEntity(); }}>
-              {this.getIntlMessage('add')}
-            </a>
-          )
-        }
       </div>
     );
   },
